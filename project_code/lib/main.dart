132457a7ba0dfbfd9732aba0273f3cd38477d91b
mariausy <mariausylte@outlook.com>
2024-05-18 17:27:19 +0200

    fungerende lab11

--- project_code/lib/main.dart
+++ project_code/lib/main.dart
@@ -1,43 +1,20 @@
 import 'package:flutter/material.dart';
-import 'package:project_code/screens/homePage.dart';
-import 'package:project_code/screens/loginPage.dart';
-import 'package:shared_preferences/shared_preferences.dart';
+import 'package:provider/provider.dart';
+import 'package:visualize_steps/providers/data_provider.dart';
+import 'package:visualize_steps/screens/homepage.dart';
 
 void main() {
   runApp(MyApp());
 } //main
 
 class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
+  MyApp({Key? key}) : super(key: key);
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      //Now the home becomes a FutureBuilder: we need to wait for the instance of SharedPreferences
-      home: FutureBuilder(
-        future: SharedPreferences.getInstance(),
-        builder: (context, snapshot) {
-          //If the instance is ready...
-          if (snapshot.hasData) {
-            //...get the instance
-            final sharedPreferences = snapshot.data!;
-            //Check if the flag isUserLogged exist...
-            if (sharedPreferences.getBool('isUserLogged') != null) {
-              //..if so, go directly to HomePage
-              return HomePage();
-            } //if
-            else {
-              //...otherwise go to LoginPage
-              return LoginPage();
-            } //else
-          } //if
-          else {
-            //While the instance of SharedPreferences is loading, just show a CircularProgress indicator in the Center
-            return Scaffold(
-              body: Center(child: CircularProgressIndicator()),
-            );
-          } //else
-        },
-      ),
+        home: ChangeNotifierProvider(
+          create: (context) => DataProvider(),
+          child: HomePage()),
     );
   } //build
-} //MyApp
+}//MyApp
