d497e645bba1f5095c0935e92a516c00f2eb7a09
mariausy <mariausylte@outlook.com>
2024-05-29 12:56:29 +0200

    Updatet stress level formula

--- project_code/lib/screens/stress_page.dart
+++ project_code/lib/screens/stress_page.dart
@@ -19,14 +19,32 @@ class StressPage extends StatelessWidget {
               mainAxisSize: MainAxisSize.min,
               children: [
                 Icon(Icons.sentiment_very_satisfied, size: 50, color: Colors.green),
-                Text('Low Stress', style: TextStyle(fontSize: 24)),
+                Text('Very Low Stress', style: TextStyle(fontSize: 24)),
                 Text('Your stress level is really good, keep it like that!',style: TextStyle(fontSize: 18)),
               ],
             ),
           ),
         ),
       );
-    } else if (stressLevel == 2) {
+      } else if (stressLevel == 2) {
+      bodyContent = Center(
+        child: Card(
+          color: Colors.orange[100],
+          child: const Padding(
+            padding: EdgeInsets.all(16.0),
+            child: Column(
+              mainAxisSize: MainAxisSize.min,
+              children: [
+                Icon(Icons.sentiment_satisfied, size: 50, color: Colors.orange),
+                Text('Low Stress', style: TextStyle(fontSize: 24)),
+                Text('Keep up your good habits and make sure you get enough rest.',style: TextStyle(fontSize: 18)),
+              ],
+            ),
+          ),
+        ),
+      );
+
+    } else if (stressLevel == 3) {
       bodyContent = Center(
         child: Card(
           color: Colors.orange[100],
@@ -43,7 +61,7 @@ class StressPage extends StatelessWidget {
           ),
         ),
       );
-    } else if (stressLevel == 3) {
+    } else if (stressLevel == 4) {
       bodyContent = Center(
         child: Card(
           color: Colors.red[100],
@@ -54,7 +72,24 @@ class StressPage extends StatelessWidget {
               children: [
                 Icon(Icons.sentiment_very_dissatisfied, size: 50, color: Colors.red),
                 Text('High Stress', style: TextStyle(fontSize: 24)),
-                Text('Your stress level is very high, have you tried meditation?',style: TextStyle(fontSize: 18)),
+                Text('Your stress level is high, have you tried meditation?',style: TextStyle(fontSize: 18)),
+              ],
+            ),
+          ),
+        ),
+      );
+      } else if (stressLevel == 5) {
+      bodyContent = Center(
+        child: Card(
+          color: Colors.red[100],
+          child: const Padding(
+            padding: EdgeInsets.all(16.0),
+            child: Column(
+              mainAxisSize: MainAxisSize.min,
+              children: [
+                Icon(Icons.sentiment_very_dissatisfied, size: 50, color: Colors.red),
+                Text('Very High Stress', style: TextStyle(fontSize: 24)),
+                Text('Your stress level is very high, try to reduce your work-load and seek support if you need it<3',style: TextStyle(fontSize: 18)),
               ],
             ),
           ),
